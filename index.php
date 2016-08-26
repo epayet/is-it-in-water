38e9df01231d20a1f3c6c2bf26905e24e76f0637
epayet <jakspinning@gmail.com>
2016-08-26 15:26:46 +0100

    Update example positions

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ require('water.php');
 
 $apiKey = "Your key";
 // Water
-// $lat = 51.096994;
-// $lng = -2.373197;
+$lat = 51.505393;
+$lng = -0.168609;
 
 // No Water
-$lat = 51.247651;
-$lng = 1.249331;
+// $lat = 51.247651;
+// $lng = 1.249331;
 
 echo "Starting with lat: ".$lat.", lng: ".$lng;
 echo "Is water: ".isItWater($lat, $lng, $apiKey);
